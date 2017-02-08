f6f2a5860f4d93b61536d9e74e1cf017f70156b9
Ben McKibben <benmckibben@uchicago.edu>
2017-02-07 19:27:57 -0600

    a few prediction tests

--- divassist/divassist_web/tests.py
+++ divassist/divassist_web/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
+from .models import Prediction, Station
+
 # Create your tests here.
+
+"""
+Tests for the Prediction model.
+"""
+class PredictionTests(TestCase):
+
+    def test_prediction_creation(self):
+        test_station = Station(station_name="Test Station", station_address="100 Test St.")
+        # test this is valid input
+        self.assertIsInstance(Station(station=test_station, bikes_available=4.87, day_of_week="Mon", start_hour=5), Station)
+        # test invalid inputs
+        with self.assertRaises(Exception):
+            test_station = Station(station=test_station, bikes_available=4.87, day_of_week="Monday", start_hour=5)
+        with self.assertRaises(Exception):
+            test_station = Station(station=test_station, bikes_available=4.87, day_of_week="Monday", start_hour=25)
+        with self.assertRaises(Exception):
+            test_station = Station(station=test_station, bikes_available=4.87, day_of_week="Mon", start_hour=-4)
+        with self.assertRaises(Exception):
+            test_station = Station(station=test_station, bikes_available=-4.3, day_of_week="Mon", start_hour=5)
