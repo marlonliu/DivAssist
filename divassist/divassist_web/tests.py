bdcbb437f0f28c39d300210bb8260bbf99225586
Ben McKibben <benmckibben@uchicago.edu>
2017-02-09 10:54:07 -0600

    better and correct prediction tests

--- divassist/divassist_web/tests.py
+++ divassist/divassist_web/tests.py
@@ -11,14 +11,21 @@ class PredictionTests(TestCase):
 
     def test_prediction_creation(self):
         test_station = Station(station_name="Test Station", station_address="100 Test St.")
-        # test this is valid input
-        self.assertIsInstance(Station(station=test_station, bikes_available=4.87, day_of_week="Mon", start_hour=5), Station)
-        # test invalid inputs
-        with self.assertRaises(Exception):
-            test_station = Station(station=test_station, bikes_available=4.87, day_of_week="Monday", start_hour=5)
-        with self.assertRaises(Exception):
-            test_station = Station(station=test_station, bikes_available=4.87, day_of_week="Monday", start_hour=25)
-        with self.assertRaises(Exception):
-            test_station = Station(station=test_station, bikes_available=4.87, day_of_week="Mon", start_hour=-4)
-        with self.assertRaises(Exception):
-            test_station = Station(station=test_station, bikes_available=-4.3, day_of_week="Mon", start_hour=5)
+        test_station.save()
+        # test model initialization and saving
+        test_prediction = Prediction(station=test_station, bikes_available=4.87, day_of_week="Mon", start_hour=5)
+        test_prediction.save()
+        # test setters
+        # set day_of_week
+        self.assertFalse(test_prediction.set_day_of_week("Mond")) # invalid day
+        self.assertTrue(test_prediction.set_day_of_week("Wed")) # valid day
+        self.assertIs(test_prediction.day_of_week, "Wed") # check setter worked
+        # set start_hour
+        self.assertFalse(test_prediction.set_start_hour(24)) # invalid hour
+        self.assertFalse(test_prediction.set_start_hour(-1)) # invalid hour
+        self.assertTrue(test_prediction.set_start_hour(23)) # valid hour
+        self.assertIs(test_prediction.start_hour, 23) # check setter worked
+        # set bikes_available
+        self.assertFalse(test_prediction.set_bikes_available(-0.1)) # invalid num bikes
+        self.assertTrue(test_prediction.set_bikes_available(2.98)) # valid num bikes
+        self.assertIs(test_prediction.bikes_available, 2.98) # check setter worked
