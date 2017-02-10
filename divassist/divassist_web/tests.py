fd4136d02952df60fc52bbe154def6a1419bb38c
marlonliu <liuwenxuan@uchicago.edu>
2017-02-09 20:32:55 -0600

    user creation tests

--- divassist/divassist_web/tests.py
+++ divassist/divassist_web/tests.py
@@ -1,8 +1,64 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.utils import timezone
 from .models import Station, UserProfile, Ride, Tag, Stop, Ride_Review, Station_Review, Ride_Rating, Station_Rating
 
 # Create your tests here.
+
+class UserAuthenticationTests(TestCase):
+    def set_up(self):
+        self.client = Client()
+
+    def test_template_availability(self):
+        self.assertEqual(self.client.get('/').status_code, 200)
+        self.assertEqual(self.client.get('/register/').status_code, 200)
+        self.assertEqual(self.client.get('/registration/select_home_station/').status_code, 200)
+
+    def test_user_creation(self):
+        test_client = Client()
+        # registration arguments are invalid, page should not redirect
+        # if two passwords are not the same
+        response = test_client.post('/register/', {'username': 'tom', 
+                                                   'email': 'a@gmail.com', 
+                                                   'password1': 'tomtom2', 
+                                                   'password2': 'tomtom'})
+        self.assertEqual(response.status_code, 200);
+        # if email address seems invalid
+        response = test_client.post('/register/', {'username': 'tom', 
+                                                   'email': 'a', 
+                                                   'password1': 'tomtom', 
+                                                   'password2': 'tomtom'})
+        self.assertEqual(response.status_code, 200);
+        # if any field is empty
+        response = test_client.post('/register/', {'username': '', 
+                                                   'email': 'a@gmail.com', 
+                                                   'password1': 'tomtom', 
+                                                   'password2': 'tomtom'})
+        self.assertEqual(response.status_code, 200);
+        response = test_client.post('/register/', {'username': 'tom', 
+                                                   'email': '', 
+                                                   'password1': 'tomtom', 
+                                                   'password2': 'tomtom'})
+        self.assertEqual(response.status_code, 200);
+        response = test_client.post('/register/', {'username': 'tom', 
+                                                   'email': 'a@gmail.com', 
+                                                   'password1': '', 
+                                                   'password2': 'tomtom'})
+        self.assertEqual(response.status_code, 200);
+        response = test_client.post('/register/', {'username': 'tom', 
+                                                   'email': 'a@gmail.com', 
+                                                   'password1': 'tomtom2', 
+                                                   'password2': ''})
+        self.assertEqual(response.status_code, 200);
+
+        # page should direct if all arguments are valid
+        response = test_client.post('/register/', {'username': 'tom', 
+                                                   'email': 'a@gmail.com', 
+                                                   'password1': 'tomtom', 
+                                                   'password2': 'tomtom'})
+        self.assertEqual(response.status_code, 302);
+        self.assertRedirects(response, '/registration/select_home_station/')
+
+
 
 class RideTests(TestCase):
     def test_ride_creation(self):
