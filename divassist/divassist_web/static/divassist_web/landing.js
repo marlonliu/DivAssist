3661c55b548f5fd99e509afb03dc10f2c5d0b87d
josh-mutian <josh-mutian@users.noreply.github.com>
2017-02-28 12:27:47 -0600

    Home landing page finished.

--- divassist/divassist_web/static/divassist_web/landing.js
+++ divassist/divassist_web/static/divassist_web/landing.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
-    $()
+    var setGreeting = function() {
+        var d = new Date();
+        var h = d.getHours();
+        var word;
+        var time;
+        if (h >= 6 && h <= 11) {
+            word = "morning";
+            time = 0;
+        } else if (h >= 12 && h <= 17) {
+            word = "afternoon";
+            time = 1;
+        } else {
+            word = "evening";
+            time = 2;
+        }
+        $("#greeting-time").text(word);
+    }
+
+    // Set greeting and landing page based on time.
+    setGreeting();
 });
